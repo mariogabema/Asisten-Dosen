439a244367458bdb5e3de93bd53b2def60263f75
raihan <raihanp0308@gmail.com>
2023-03-14 13:17:42 +0700

    Praktikum 3

---
+++ Desain dan Analisis Algoritma/Praktikum-1/coba.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+
+using namespace std;
+
+int main() {
+	string nama[3] = {"Ayu", "Dani", "Hera"};
+	
+	for (int i = 0; i < 3; i++) {	
+		cout << "Nama mahasiswa: " + nama[i] << endl;
+ 	}
+
+}
