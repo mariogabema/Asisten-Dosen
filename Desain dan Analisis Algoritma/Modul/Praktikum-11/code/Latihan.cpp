8d60adeb0586187a03e2303b91f7f45684251db4
raihan <raihanp0308@gmail.com>
2023-06-08 17:13:32 +0700

    Update latihan

--- Desain dan Analisis Algoritma/Modul/Praktikum-11/code/Latihan.cpp
+++ Desain dan Analisis Algoritma/Modul/Praktikum-11/code/Latihan.cpp
@@ -2,22 +2,26 @@
 
 using namespace std;
 
-class Latihan {
-	private:
-		int nilai;
+class Latihan { // class name
+	private: // access specifier
+		int nilai; // attribut private
 	
 	public:
+		// constructor
 		Latihan(int n) {
-			nilai = n;
+			nilai = n; // pemberian suatu nilai kedalam attribut
 		}
 		
+		// getter
 		int getNum() {
 			return nilai;
 		}
 };
 
 int main() {
+	// instance object
 	Latihan lat(10);
+	// return method getter
 	cout << "Nilai yang diinputkan: " << lat.getNum() << endl;
 	
 	return 0;
